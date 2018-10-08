02ccdce582f03f45d35dfc7cb7041ad26091da59
Goose <kalinarepina@gmail.com>
2018-10-08 16:06:19 +0300

    Добавлено несколько кодов клавишь

--- Bombermen/Controller.cpp
+++ Bombermen/Controller.cpp
@@ -3,8 +3,35 @@
 
 #include "Model.h"
 
-void display(unsigned char key, int x, int y)
+#define VK_UP_PLAYER1 1
+#define VK_DOWN_PLAYER1 12
+#define VK_RIGHT_PLAYER1 13
+#define VK_LEFT_PLAYER1 14
+
+void KeyboardUpFunc(unsigned char key, int x, int y)
 {
+	int a = 5;
+	switch (key)
+	{
+		case VK_UP_PLAYER1:
+		{
+			break;
+		}
+		case VK_DOWN_PLAYER1:
+		{
+			break;
+		}
+		case VK_RIGHT_PLAYER1:
+		{
+			break;
+		}
+		case VK_LEFT_PLAYER1:
+		{
+			break;
+		}
+		default:
+			break;
+	}
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glBegin(GL_TRIANGLES);
@@ -16,7 +43,7 @@ void display(unsigned char key, int x, int y)
 	glutSwapBuffers();
 }
 void MainLoop() {
-	glutKeyboardUpFunc(display);
+	glutKeyboardUpFunc(KeyboardUpFunc);
 	
 }
 
